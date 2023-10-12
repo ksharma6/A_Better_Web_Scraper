cbb18121de10867a43bfd23c4b6d42afe2521546
ksharma6 <ksharma6@tutanota.com>
2023-10-12 11:31:15 -0700

    added import json

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-
+from src.neetcode_scraper import NeetCodeScraper
+import json
 
 if __name__ == '__main__':
 
     with open('config.json') as config_file:
         data = json.load(config_file)
 
-    bot = EasyApplyLinkedin(data)
-    bot.apply()
+    bot = NeetCodeScraper(data)
+    bot.scrape_static_page()
+    bot.scrape_dynamic_page()
+    bot.write_2_csv()
